fe75ef9d7467af17a67157ba5a6ca48b9d319741
thijsmeijer <t.meijer@ifresh.nl>
2022-12-19 17:47:26 +0100

    PAV-013 change profile route to a singleton route

--- routes/web.php
+++ routes/web.php
@@ -30,10 +30,8 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update')->middleware([HandlePrecognitiveRequests::class]);
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware(['auth', HandlePrecognitiveRequests::class])->group(function () {
+    Route::singleton('profile', ProfileController::class);
 });
 
 require __DIR__.'/auth.php';
